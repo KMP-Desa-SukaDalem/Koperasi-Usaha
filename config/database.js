b58ce46f68cbbecf9b31a8b573556190116ef43d
Rama Aryo <ramaaryoprambudi@gmail.com>
2026-05-10 12:34:52 +0700

    remove:server_output.log

--- config/database.js
+++ config/database.js
@@ -5,9 +5,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'koperasi_desa_db',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
